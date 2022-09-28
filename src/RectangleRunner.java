215c0ccb781fe51a099a11e18b9cf923b8a557a1
1e10 Laptop <sample@bths.edu>
2022-09-28 12:23:36 -0400

    commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter rectangle 1 length: ");
+        int rect1Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 width: ");
+        int rect1Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 height: ");
+        double rect1Height = myScanner.nextDouble();
+        System.out.print("Enter rectangle 2 length: ");
+        int rect2Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 width: ");
+        int rect2Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 height: ");
+        double rect2Height = myScanner.nextDouble();
+
+        // write the rest of your program below
+    }
+}
